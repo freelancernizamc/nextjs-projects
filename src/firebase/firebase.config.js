e370afd3d75575dc82ae7eda32befa4474e3cbcc
freelancernizamc <nizamuddin2020ctg@gmail.com>
2023-11-22 19:28:19 +0600

    Firebase installed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAYjbl_KIfn48aBij9tLP7NS1fU37ttJkw",
+  authDomain: "practice-nextjs-e526f.firebaseapp.com",
+  projectId: "practice-nextjs-e526f",
+  storageBucket: "practice-nextjs-e526f.appspot.com",
+  messagingSenderId: "15123693812",
+  appId: "1:15123693812:web:94ebb57f35177d94a45ea1"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
